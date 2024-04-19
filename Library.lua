a07b97b9bb5247b4d1a873cc00204bcc5b2db306
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2024-04-19 20:05:45 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -11,12 +11,13 @@ local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
+local GetHUI = gethui or (function() return CoreGui end);
 
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
-ScreenGui.Parent = CoreGui;
+ScreenGui.Parent = GetHUI();
 
 local Toggles = {};
 local Options = {};
